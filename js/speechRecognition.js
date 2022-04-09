62a4fc9ae815d46bab4c1ca65c0a9a196d068521
Nitya Kasturey <67405598+NityaKasturey01@users.noreply.github.com>
2022-04-09 16:01:28 +0530

    Update speechRecognition.js

--- js/speechRecognition.js
+++ js/speechRecognition.js
@@ -1,41 +1,29 @@
 if ("webkitSpeechRecognition" in window) {
-    // Initialize webkitSpeechRecognition
     let speechRecognition = new webkitSpeechRecognition();
-  
-    // String for the Final Transcript
     let final_transcript = "";
   
-    // Set the properties for the Speech Recognition object
     speechRecognition.continuous = true;
     speechRecognition.interimResults = true;
-    //speechRecognition.lang = document.querySelector("#select_dialect").value;
-    speechRecognition.lang = 'hi-IN'
-    
-    
-    
+   
+    speechRecognition.lang = 'hi-IN' 
+    speechRecognition.maxAlternatives = 3;
     console.log(speechRecognition.lang)
   
-    // Callback Function for the onStart Event
     speechRecognition.onstart = () => {
-      // Show the Status Element
-      document.querySelector("#status").style.display = "block";
+      document.querySelector("#listentingText").style.display = "block";
     };
     speechRecognition.onerror = () => {
-      // Hide the Status Element
-      document.querySelector("#status").style.display = "none";
+      document.querySelector("#listentingText").style.display = "none";
     };
     speechRecognition.onend = () => {
-      // Hide the Status Element
-      document.querySelector("#status").style.display = "none";
+      document.querySelector("#listentingText").style.display = "none";
     };
   
     speechRecognition.onresult = (event) => {
-      // Create the interim transcript string locally because we don't want it to persist like final transcript
       let interim_transcript = "";
+     console.log(event.results.transcript)
   
-      // Loop through the results from the speech recognition object.
       for (let i = event.resultIndex; i < event.results.length; ++i) {
-        // If the result item is Final, add it to Final Transcript, Else add it to Interim transcript
         if (event.results[i].isFinal) {
           final_transcript += event.results[i][0].transcript;
         } else {
@@ -43,19 +31,14 @@ if ("webkitSpeechRecognition" in window) {
         }
       }
   
-      // Set the Final transcript and Interim transcript.
       document.querySelector("#final").innerHTML = final_transcript;
       document.querySelector("#interim").innerHTML = interim_transcript;
     };
   
-    // Set the onClick property of the start button
     document.querySelector("#start").onclick = () => {
-      // Start the Speech Recognition
       speechRecognition.start();
     };
-    // Set the onClick property of the stop button
     document.querySelector("#stop").onclick = () => {
-      // Stop the Speech Recognition
       speechRecognition.stop();
     };
   } else {
